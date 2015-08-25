55e808d3c0084d16d1ed04c36a6f5a5ecad4de08
rritoch <rritoch@gmail.com>
2015-08-25 12:00:12 +0800

    Release v0.0.2

--- impl/src/main/java/org/armedbear/lisp/Version.java
+++ impl/src/main/java/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "0.0.2-SNAPSHOT";
+  static final String baseVersion = "0.0.2";
   
   static void init() {
     try {
